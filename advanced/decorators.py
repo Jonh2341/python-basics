271e1f639170453bee77dcda209525368ee8c31b
Jonh2341 <rokakaak@gmail.com>
2026-04-05 17:13:51 +0300

    add exception in decorator

--- advanced/decorators.py
+++ advanced/decorators.py
@@ -1,6 +1,13 @@
 def add_something(func):
     def wrapper(*args, **kwards):
+        # define addition
         addition = args[0] if args[0] != '' else 'standart'
+
+        # exception for digits
+        if any(char.isdigit() for char in addition):
+            raise ValueError('Digits are not allowed!')
+
+        # checking the addition ingridient
         if addition == 'standart':
             print('you add nothing')
         else:
@@ -8,6 +15,7 @@ def add_something(func):
         return func(addition, *args[1:], **kwards)
     return wrapper
 
+# decorator
 @add_something
 def pasta(condition):
     print(f'there is your {condition} pasta!')
