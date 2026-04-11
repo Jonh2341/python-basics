4a5a2f6a1244d8e1eb7e60f3af4852bd193182f8
Jonh2341 <rokakaak@gmail.com>
2026-04-11 19:32:55 +0300

    about post in flask (forms)

--- advanced/flaskPy.py
+++ advanced/flaskPy.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, Flask 67"
+    return f'/ --> /form '
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        username = request.form["username"]
+        return f'Received: {username}'
+    return '''
+        <form method="post">
+            <input name="username">
+            <input type="submit">
+        </form>
+    '''
 
 if __name__ == "__main__":
     app.run(debug=True)
